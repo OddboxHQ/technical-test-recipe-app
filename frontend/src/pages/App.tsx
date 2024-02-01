225f225e5242029796f1ebbcafc6aa77947d93ba
Jack Mullen <jack.mullen@oddbox.co.uk>
2024-02-01 14:53:56 +0000

    Add boilerplate for frontend app (#2)
    * added front-end foundations and docker compose file
    
    * improved readme on how to get started
    
    * fixed imports

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from '../components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <section className="App-body">
+        <h1>Welcome to the Oddbox Recipe Finder!</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
